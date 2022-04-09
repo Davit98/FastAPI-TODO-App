82f03cc1702ab7e0986bb794c193ccd80b9e3cea
Davit Martirosyan <davitmartirosyan@Davits-MacBook-Pro-2.local>
2022-04-09 15:32:00 +0400

    first commit

---
+++ TodoApp/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Dmass#2022@127.0.0.1:3306/todoapp"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False}
+# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+
+Base = declarative_base()
