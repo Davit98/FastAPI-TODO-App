fa0c9d9d270f213535589aadac58ea4c426c2631
Davit98 <dvmartirosyan@gmail.com>
2022-04-10 21:50:33 +0400

    show list of Todos for a user implement initial logics for the 'Add a new Todo' button

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -2,15 +2,15 @@ import sys
 
 sys.path.append('..')
 
-from typing import Optional
+from starlette.responses import RedirectResponse
+from starlette import status
 
-from fastapi import Depends, HTTPException, APIRouter, Request
-from pydantic import BaseModel, Field
+from fastapi import Depends, APIRouter, Request, Form
 from sqlalchemy.orm import Session
 
 import models as models
 from database import engine, SessionLocal
-from .auth import get_current_user, get_user_exception
+
 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -34,13 +34,31 @@ def get_db():
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
+
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...), description: str = Form(...),
+                      priority: int = Form(...), db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
